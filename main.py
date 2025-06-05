79d3055b6ef7848b902de3ac64190683ea824c5a
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-06-05 15:32:57 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi import FastAPI, Query, HTTPException
 import json
 
 app = FastAPI()
@@ -10,34 +10,33 @@ def load_data():
 
 @app.get("/")
 def sample():
-    return f"hi my name is sandeep "
+    return {"message": "hi my name is sandeep"}
 
 @app.get("/hellow")
 def hello():
-    return f"hello"
+    return {"message": "hello"}
 
 @app.get("/view")
 def views():
-
     data = load_data()
     return data
 
-
-
 @app.get("/sort")
-def sort_data(sort_by: str = Query(..., description='sort by the basic of density, residual_sugar'),order: str = Query('asc')):
-    valid_file =['density', 'residual_sugar']
+def sort_data(
+    sort_by: str = Query(..., description='Sort by "density" or "residual_sugar"'),order: str = Query('asc')):
+    
+    valid_fields = ['density', 'residual_sugar']
 
-    if sort_by not in  valid_file:
-        return HTTPException(status_code=400)
-    
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail="Invalid sort field.")
+
     if order not in ['asc', 'desc']:
-        return HTTPException(status_code=400)
-    
+        raise HTTPException(status_code=400, detail="Invalid order. Use 'asc' or 'desc'.")
+
     data = load_data()
 
-    sort_data =  True if order == 'desc' else False
-    
-    sorted_data = sorted(data.values(), key=lambda x:x.get('sort_by', 0), reverse=sort_data)
+    reverse = True if order == 'desc' else False
+
+    sorted_data = sorted(data, key=lambda x: x.get(sort_by, 0), reverse=reverse)
 
     return sorted_data
