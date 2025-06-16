290c1aaa9da89977858dfe5775a1cd62f0fcbf07
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-06-16 19:23:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ app = FastAPI()
 
 def load_data():
     with open('data.json', 'r') as file:
-        data = json.load(file)
-        
+        data = json.load(file)  
         return data
 
 @app.get("/")
